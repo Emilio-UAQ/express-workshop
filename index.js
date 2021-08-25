26ff08f0187b63139dc6d5f09d46832105839d86
Emilio GR <wuisemi_@hotmail.com>
2021-08-25 17:36:43 -0500

    added README/pokedex json and modified index/package/package lock

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require( 'express');
 const app = express();
+const { pokemon } = require( './pokedex.json');
 
-app.get("/", (req, res, next) => {
-  res.status(200);
-  res.send("Bienvenido");
+app.get('/pokemon', (req, res, next) => {
+  res.status (200);
+  res.send("Bienvenido a la Pokédex");
 });
 
-app.listen (3000, () => {
+app.get('/pokemon/all', (req, res, next) => {
+  res.status (200);
+  res.send(pokemon);
+});
+
+app.get('/pokemon/:id([0-9]{1-3})', (req, res, next) => {
+  const id = req.params.id - 1;
+  if(id >= 0 && id <= 150) {
+    res.status (200);
+    return res.send(pokemon [req.params.id - 1]);
+  }
+  res.status (404);
+  res.send("Pokémon no encontrado");
+});
+
+app.get('/pokemon/:name', (req, res, next) => {
+  const name = req.params.name;
+  let bool = false;
+  for(i = 0; i < pokemon.length; i++) {
+    if(pokemon[i].name == name) {
+      bool = true;
+      res.status (200);
+      return res.send(pokemon[i]);
+    }
+  }
+  res.status (404);
+  res.send("Pokémon no encontrado");
+});
+
+app.listen (process.env.PORT || 3000, () => {
   console.log("Server is running...");
 });
